3b261490830bd4fdde6dafba9d377907ca3c902f
me <me@DESKTOP-732RL7E>
2017-12-02 17:25:12 +0200

    Add project files.

---
+++ FirestProg1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirestProg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Hello world");
+            Console.Write("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
